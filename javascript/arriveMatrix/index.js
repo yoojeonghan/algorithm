ce41b572c68cd8d5c404185e5b532bb055ec9708
ehfcn1234 <ehfcn1234@gmail.com>
2020-07-10 14:11:33 +0900

    arriveMatrix

---
+++ javascript/arriveMatrix/index.js
@@ -0,0 +1,39 @@
+function solution(matrix, cost, m, n) {
+    let answer = 0;
+    const moveArray = [[1, 0], [0, 1]];
+    const checkedArray = [];
+    for (let i = 0; i < n; i++) {
+        const array = [];
+        for (let j = 0; j < m; j++) {
+            array.push(false);
+        }
+        checkedArray.push(array);
+    }
+    checkedArray[0][0] = true;
+
+    dfs([0, 0], cost - matrix[0][0], [[0, 0], matrix[0][0]], checkedArray);
+
+    function dfs(start, remainCost, points, checked) {
+        if (start[0] == matrix.length - 1 && start[1] == matrix.length - 1 && remainCost == 0) {
+            answer++;
+        }
+        for (let i = 0; i < 2; i++) {
+            const nextPoint = [start[0] + moveArray[i][0], start[1] + moveArray[i][1]];
+            if (nextPoint[0] >= 0 && nextPoint[1] >= 0 && nextPoint[0] < m && nextPoint[1] < n) {
+                if (!checked[nextPoint[0]][nextPoint[1]]) {
+                    const currentCost = remainCost - matrix[nextPoint[0]][nextPoint[1]];
+                    if (currentCost >= 0) {
+                        const temp = JSON.parse(JSON.stringify(checked));
+                        temp[nextPoint[0]][nextPoint[1]] = true;
+                        const temp2 = JSON.parse(JSON.stringify(points));
+                        temp2.push([nextPoint[0], nextPoint[1]], matrix[nextPoint[0]][nextPoint[1]]);
+                        dfs([nextPoint[0], nextPoint[1]], currentCost, temp2, temp);
+                    }
+                }
+            }
+        }
+    }
+    return answer;
+}
+
+module.exports = solution;
---
+++ javascript/arriveMatrix/test.js
@@ -0,0 +1,8 @@
+const solution = require('./index');
+const asssert = require('assert');
+
+describe('solution Test', () => {
+    it('[[4,7,1,6], [5,7,3,9], [3,2,1,2], [7,1,6,3]], 25, 4, 4의 경우 두 가지 경로로 도달할 수 있다.', function() {
+        asssert.equal(solution([[4,7,1,6], [5,7,3,9], [3,2,1,2], [7,1,6,3]], 25, 4, 4), 2);
+    });
+});
