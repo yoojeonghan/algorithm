ab5898a28f1cdcef5a61119905afbb0c44d4cab3
yoojeonghan <yoojeonghan@gmail.com>
2020-07-30 22:58:12 +0900

    blindPhoneNumber

---
+++ cpp/blindPhoneNumber/main.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+string solution(string phone_number) {
+    string answer = "";
+    for (int i = 0; i < phone_number.length() - 4; i++) {
+        answer += "*";
+    }
+    for (int i = phone_number.length() - 4; i < phone_number.length(); i++) {
+        answer += phone_number[i];
+    }
+    return answer;
+}
