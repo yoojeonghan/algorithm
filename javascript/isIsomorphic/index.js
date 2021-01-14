7136b219ef47c587418a9e6b3565aa1841e8ae8a
ehfcn1234 <ehfcn1234@gmail.com>
2021-01-15 00:43:00 +0900

    isIsomorphic

---
+++ javascript/isIsomorphic/index.js
@@ -0,0 +1,48 @@
+/**
+ * Given two strings s and t, determine if they are isomorphic.
+ * Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+ * All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+ */
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+    s = s.split("");
+    t = t.split("");
+    
+    let nums = 0;
+    let numt = 0;
+    for (let i = 0; i < s.length; i += 1) {
+        s = s.map((item) => { 
+            if (!Number.isInteger(item) && item == s[i]) {
+                nums += 1;
+                return i;
+            }
+            return item;
+        });
+        if (nums === s.length) {
+            break;
+        }
+    }
+    
+    for (let i = 0; i < t.length; i += 1) {
+        t = t.map((item) => { 
+            if (!Number.isInteger(item) && item == t[i]) {
+                numt += 1;
+                return i;
+            }
+            return item;
+        });
+        if (numt === t.length) {
+            break;
+        }
+    }
+        
+    return s.join("") === t.join("");
+};
+/**
+ * Runtime: 324 ms, faster than 5.12% of JavaScript online submissions for Isomorphic Strings.
+ * Memory Usage: 51.7 MB, less than 5.12% of JavaScript online submissions for Isomorphic Strings.
+ */
